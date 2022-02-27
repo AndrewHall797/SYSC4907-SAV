3ce84a3b204875ed3deb6bab4068583525fa2e95
AndrewHall797 <andrew.hall286@gmail.com>
2022-02-27 17:48:05 -0500

    Basic navigation metric

--- src/mapping_navigation/scripts/navigation.py
+++ src/mapping_navigation/scripts/navigation.py
@@ -2,6 +2,7 @@
 
 import rospy
 import os
+import numpy as np
 from geometry_msgs.msg import PoseStamped
 from pure_pursuit import *
 from std_msgs.msg import Float64
@@ -19,6 +20,8 @@ class Navigation:
         self.target_index = 0
         self.navigator = PurePursuit(look_ahead_distance, look_forward_gain, path)
         self.car_state = CarState(wheel_base)
+        self.previous_point = 0
+        self.car_dist = []
 
     def listener(self):
         rospy.init_node('navigation', anonymous=True)
@@ -34,6 +37,11 @@ class Navigation:
                 self.steering_pub.publish(steering_angle)
             else:
                 rospy.loginfo('Done route')
+                with open(os.path.abspath(os.path.dirname(__file__)) + '/nav_metric.txt', 'w') as metrics_file:
+                    metrics_file.write(f"Average distance to the optimal path is {np.average(self.car_dist)}\n")
+                    metrics_file.write(f"Median distance to the optimal path is {np.median(self.car_dist)}\n")
+                    metrics_file.write(f"Max distance along the entire trip is {np.max(self.car_dist)}")
+                    rospy.loginfo('wrote')
                 # rospy.sigal_shutdown("Done route")
             rate.sleep()
 
@@ -51,11 +59,31 @@ class Navigation:
     """
 
     def get_steering_angle(self) -> float:
-        ackerman_angle_rad, self.target_index = self.navigator.pure_pursuit_steer_control(self.car_state)
+        ackerman_angle_rad, new_target_index = self.navigator.pure_pursuit_steer_control(self.car_state)
+
+        # If a new goal point is selected set the previous point to the last point
+        if new_target_index != self.target_index:
+            self.previous_point = self.target_index
+
+        self.target_index = new_target_index
         ackerman_angle_ratio = math.degrees(ackerman_angle_rad) / 45
-        rospy.loginfo(f"""Target point is {path[self.target_index].coordinate[0]}, {path[self.target_index].coordinate[1]},
+        rospy.loginfo(
+            f"""Target point is {path[self.target_index].coordinate[0]}, {path[self.target_index].coordinate[1]},
                       car position {self.car_state.point.coordinate[0]}, {self.car_state.point.coordinate[1]}""")
         rospy.loginfo(f'Current steering angle in degrees {math.degrees(ackerman_angle_rad)}')
+
+        # Calc the distance of that car to the points
+        if self.target_index != self.previous_point:
+            cur_pos = (self.car_state.point.coordinate[0], self.car_state.point.coordinate[1])
+            previous_cord = (path[self.target_index].coordinate[0], path[self.target_index].coordinate[1])
+            target_cord = (path[self.previous_point].coordinate[0], path[self.previous_point].coordinate[1])
+
+            distance = np.abs(
+                np.cross(np.subtract(target_cord, previous_cord), np.subtract(previous_cord, cur_pos))) \
+                       / np.linalg.norm(np.subtract(target_cord, previous_cord))
+            rospy.loginfo(f'Distance {distance}')
+            self.car_dist.append(distance)
+
         return ackerman_angle_ratio
 
 
