69009321442416df178e35dd42219255d5caf104
AndrewHall797 <andrew.hall286@gmail.com>
2022-03-12 16:51:49 -0500

    fix issue with enum label for warnings

--- src/mapping_navigation/scripts/road_warning.py
+++ src/mapping_navigation/scripts/road_warning.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 
 class RoadWarning(Enum):
-    STRAIGHT_ROAD_AHEAD = 2
-    INTERSECTION_AHEAD = 0
+    STRAIGHT_ROAD_AHEAD = 0
     TURN_AHEAD = 1
+    INTERSECTION_AHEAD = 2
     SAME_AHEAD = 3
